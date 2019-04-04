c4e2a981372f8d94eccdb49b496631bd750f8af9
Mariusz Mickiewicz <mariusz.mickiewicz@nokia.com>
2019-04-04 17:43:42 +0200

    Added representation of graph

---
+++ Graph/Graph.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <vector>
+#include <iostream>
+
+template<typename V, typename E>
+struct Graph
+{
+    struct Ed : E
+    {
+        int v;
+
+        Ed(E p, int w) : E{p}, v{w} { }
+    };
+
+    struct Ve : V, std::vector<Ed> { };
+
+    std::vector<Ve> g;
+
+    Graph(unsigned n = 0) : g(n) { }
+
+    void edgeDirected(int b, int e, E d = E {})
+    {
+        g[b].push_back(Ed{d, e});
+    }
+
+    void edgeUndirected(int b, int e, E d = E {})
+    {
+        Ed eg{d, e};
+        eg.rev = g[e].size() + (b == e);
+        g[b].push_back(eg);
+        eg.rev = g[eg.v = b].size() - 1;
+        g[e].push_back(eg);
+    }
+
+    void write()
+    {
+        for(unsigned x = 0; x < g.size(); ++x)
+        {
+            std::cout << x << ":";
+
+            for(const auto& v : g[x])
+                std::cout << " " << v.v;
+
+            std::cout << std::endl;
+        }
+    }
+};
---
+++ Graph/main.cpp
@@ -0,0 +1,35 @@
+#include "Graph.hpp"
+
+struct Empty { };
+
+int main()
+{
+    unsigned numOfVertices = 0;
+    unsigned numOfEdges = 0;
+
+    std::cout << "Num of vertices in graph: ";
+    std::cin >> numOfVertices;
+
+    std::cout << "Num of edges in graph: ";
+    std::cin >> numOfEdges;
+
+    Graph<Empty, Empty> graph {numOfVertices};
+
+    for(unsigned i = 0; i < numOfEdges; ++i)
+    {
+        unsigned edgeStart = 0;
+        unsigned edgeEnd = 0;
+
+        std::cout << i + 1 << " (out of " << numOfEdges << ") edge start: ";
+        std::cin >> edgeStart;
+
+        std::cout << i + 1 << " (out of " << numOfEdges << ") edge end: ";
+        std::cin >> edgeEnd;
+
+        graph.edgeDirected(edgeStart, edgeEnd);
+    }
+
+    graph.write();
+
+    return 0;
+}
